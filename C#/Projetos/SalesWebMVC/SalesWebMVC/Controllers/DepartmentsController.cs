31d4812ed89c654a2eab6105cd9186be03eee65d
Marcos Moreira <Marcosmorlopes@hotmail.com>
2023-01-20 19:52:56 -0300

    Commit

---
+++ C#/Projetos/SalesWebMVC/SalesWebMVC/Controllers/DepartmentsController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SalesWebMVC.Models;
+
+namespace SalesWebMVC.Controllers
+{
+    public class DepartmentsController : Controller
+    {
+        public IActionResult Index()
+        {
+            List<Department> departmentList = new List<Department>();
+            departmentList.Add(new Department { Id = 1, Name = "Department1" });
+            departmentList.Add(new Department { Id = 2, Name = "Department2" });
+            departmentList.Add(new Department { Id = 3, Name = "Department3" });
+
+            return View(departmentList);
+        }
+    }
+}
