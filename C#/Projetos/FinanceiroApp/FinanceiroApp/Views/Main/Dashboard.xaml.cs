942cbe1db2ada424f4749fb181a4f1738e6868ce
Marcos Moreira <Marcosmorlopes@hotmail.com>
2023-04-13 23:32:57 -0300

    Remoção do projeto .WPF

---
+++ C#/Projetos/FinanceiroApp/FinanceiroApp/Views/Main/Dashboard.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace FinanceiroApp.WPF.Views.Main
+{
+    /// <summary>
+    /// Interaction logic for Dashboard.xaml
+    /// </summary>
+    public partial class Dashboard : Window
+    {
+        public Dashboard()
+        {
+            InitializeComponent();
+            txtTitle.Text = "Bem-vindo " + Library.Session.User.Name;
+        }
+
+        private void btnUpdateUser_Click(object sender, RoutedEventArgs e)
+        {
+            WPF.Views.User.Register register = new User.Register(Library.Session.User);
+            register.ShowDialog();
+        }
+
+        private void btnNewTransaction_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnNewTransactionCategory_Click(object sender, RoutedEventArgs e)
+        {
+            Transaction.TransactionCategoryRegister categoryRegister = new Transaction.TransactionCategoryRegister();
+            categoryRegister.Show();
+        }
+    }
+}
---
+++ C#/Projetos/FinanceiroApp/FinanceiroApp/Views/User/Register.xaml.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Windows;
+using FinanceiroApp.Library.Exceptions;
+
+namespace FinanceiroApp.WPF.Views.User
+{
+    /// <summary>
+    /// Interaction logic for Register.xaml
+    /// </summary>
+    public partial class Register : Window
+    {
+        bool UpdatingUser = false;
+        public Library.ViewModels.UserViewModel user = new Library.ViewModels.UserViewModel();
+
+        public Register()
+        {
+            InitializeComponent();
+            this.DataContext = user;
+            LoadWindow();
+        }
+
+        public Register(Library.ViewModels.UserViewModel user)
+        {
+            InitializeComponent();
+            this.user = user;
+            this.DataContext = this.user;
+            UpdatingUser = true;
+            LoadWindow();
+        }
+
+        public void LoadWindow()
+        {
+            if (UpdatingUser)
+            {
+                this.Title = "Editar Perfil";
+                txtTitle.Text = "Editar Perfil";
+                tbNewPassword.Text = "Nova Senha";
+
+                tbCurPassword.Visibility = Visibility.Visible;
+                txtCurPassword.Visibility = Visibility.Visible;
+            }
+            else
+            {
+                txtTitle.Text = "Registrar";
+                tbNewPassword.Text = "Senha";
+            }
+        }
+
+        private void ValidateRegister()
+        {
+            if (string.IsNullOrEmpty(txtUser.Text))
+                throw new FinAppValidationException("Preencha o nome de usuário!");
+
+            if (string.IsNullOrEmpty(txtEmail.Text))
+                throw new FinAppValidationException("Preencha o email!");
+
+            if (string.IsNullOrEmpty(txtPassword.Password))
+                throw new FinAppValidationException("Preencha a senha!");
+            
+            if (UpdatingUser)
+            {
+                if (string.IsNullOrEmpty(txtCurPassword.Password))
+                    throw new FinAppValidationException("Preencha a senha atual!");
+
+                if (!string.IsNullOrEmpty(txtCurPassword.Password))
+                    user.ValidatePassword(txtCurPassword.Password);
+            }
+
+            if (string.IsNullOrEmpty(txtConfirmPassword.Password))
+                throw new FinAppValidationException("Preencha a confirmação de senha!");
+
+            if (!txtPassword.Password.Equals(txtConfirmPassword.Password))
+                throw new FinAppValidationException("A senha deve ser igual à confirmação de senha!");
+        }
+
+        private void btnRegister_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {              
+                ValidateRegister();
+                user.Password = txtPassword.Password;
+                user.SaveUser();
+
+                MessageBox.Show("Usuário salvo com sucesso!", "Registro de Usuário", MessageBoxButton.OK, MessageBoxImage.Information);
+
+                this.Close();
+            }
+            catch (FinAppValidationException rvex)
+            {
+                txtErrors.Visibility = Visibility.Visible;
+                txtErrors.Text = rvex.Message;
+            }catch(Exception ex)
+            {
+                MessageBox.Show("Erro ao salvar o usuário!", "Registro de Usuário", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+    }
+}
