612e28e06427364571af73f519b01918c0fa2e62
Marcos Moreira <Marcosmorlopes@hotmail.com>
2023-07-03 21:15:52 -0300

    Commit

--- C#/Projetos/FinanceiroApp/FinanceiroApp.WPF/Controls/Dashboard/WalletItem.xaml.cs
+++ C#/Projetos/FinanceiroApp/FinanceiroApp.WPF/Controls/Dashboard/WalletItem.xaml.cs
@@ -56,25 +56,5 @@ namespace FinanceiroApp.WPF.Controls.Dashboard
         {
             ViewModel.SetWallet(Wallet);
         }
-
-        /* TODO: AULA DE BACON
-         EMPIRISTA
-        CONHECIMENTO EH PODER
-        PRECARIEDADE DO INTELECTO
-        METODO INDUTIVO
-        FATOS PARTICULARES
-        LUTA CONTRA IDOLATRIA(VIES)
-        TABELAS DE HISTORIA NATURAL(COLETA)
-        PROGRESSO
-
-        DOUTRINA DOS IDOLOS
-
-        1 - TRIBO -> DEFICIENCIAS UNIVERSAIS
-        2 - CAVERNA
-        3 - FORO/MERCADO -> LINGUAGEM
-        4 - TEATRO -> "FABULAS DOS SISTEMAS"
-
-        HATER DA MATEMATICA E DO PITAGORAS
-         */
     }
 }
